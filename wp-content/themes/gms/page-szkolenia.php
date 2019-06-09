fe3ac4d17ea2b01274f9d9b93ba47429270adbf8
Tomasz <ttgrzybek@gmail.com>
2019-06-09 20:38:21 +0200

    Changed szkolenia and coaching to be editable

--- wp-content/themes/gms/page-szkolenia.php
+++ wp-content/themes/gms/page-szkolenia.php
@@ -1,58 +1,33 @@
 <?php get_header(); ?>
 
 <div class="background--image"></div>
-<div class="container first--content--op">
-    <div class="row first--content row--one--column">
-        <div class="col col--workshop">
-            <ul>
-                <h3>Dla każdego</h3>
-                <li>Telefoniczne umawianie spotkań</li>
-                <li>Sprzedaż i obsługa przez telefon</li>
-                <li>Techniki sprzedaży - B2C</li>
-                <li>Techniki sprzedaży - B2B</li>
-                <li>Trudne sytuacje w sprzedaży</li>
-                <li>Obsługa klienta na 6</li>
-                <li>Zarządzanie klientem kluczowym - KAM</li>
-                <li>Zarządzanie zespołem sprzedaży</li>
-                <li>Coaching sprzedaży</li>
-            </ul>
-        </div>
-        <div class="col col--workshop">
-            <ul>
-                <h3>Dla lidera</h3>
-                <li>Przywództwo w 5 krokach</li>
-                <li>Coaching dla menedżerów</li>
-                <li>Motywowanie i budowanie zaangażowania</li>
-                <li>Debiutujący menedżer</li>
-                <li>Narzędzia w pracy menedżera</li>
-                <li>Rekrutacja dla menedżerów</li>
-                <li>Ocena pracownicza </li>
-                <li>Zarządzanie Projektem: PM Practitioner, PM Master, PM Leader, PM - gra symulacyjna</li>
-                <li>Planowanie strategiczne</li>
-                <li>Zarządzanie zmianą</li>
+<div class="container first--content last--content">
 
-            </ul>
-        </div>
-        <div class="col col--workshop last--content">
-            <ul>
-                <h3>Dla handlowca</h3>
-                <li>Komunikacja interpersonalna</li>
-                <li>Asertywność</li>
-                <li>Zarządzanie czasem 5.0</li>
-                <li>Oswoić zmiany</li>
-                <li>Prezentacje i wystąpienia publiczne</li>
-                <li>Automotywacja</li>
-                <li>Warsztaty kreatywności - trening twórczości</li>
-                <li>Docenianie różnorodności</li>
-                <li>W harmonii miedzy pracą i życiem – WLB</li>
-                <li>Kształtowanie wizerunku</li>
-                <li>Sztuka wywierania wpływu</li>
-                <li>Savoir-vivre w biznesie</li>
-                <li>Życiowy kompas - poszukiwanie drogi</li>
-            </ul>
-        </div>
+    <?php
+    $q2 = new WP_Query([
+        'post_type' => 'post',
+        'category_name' => 'szkolenia',
+    ]);
+    ?>
 
-    </div>
+    <?php if ($q2->have_posts() ) : while ( $q2->have_posts() ) :    $q2->the_post(); ?>
+        <div class="row row--one--column">
+            <div class="col col--workshop post--content">
+
+                <?php the_title('<h3 class="coaching">', '</h3>');?>
+
+                <?php the_content('<h3 class="coaching">', '</h3>');?>
+                <?php wp_reset_postdata(); ?>
+            </div>
+        </div>
+    <?php endwhile; ?>
+    <?php else: ?>
+    <?php endif; ?>
+
+
 </div>
 
+
 <?php get_footer(); ?>
+
+
